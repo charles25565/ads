017184602b6b6005754e7aceb4800e9c2992695c
Charles <c4a@monocles.eu>
2025-01-25 21:07:52 -0500

    Add more ad networks and fix subdomain= syntax

--- generate.sh
+++ generate.sh
@@ -7,11 +7,14 @@ wget -q https://wordpress.com/ads.txt -O wordpress.txt &
 wget -q https://cnn.com/ads.txt -O cnn.txt &
 wget -q https://foxnews.com/ads.txt -O foxnews.txt &
 wget -q https://modrinth.com/ads.txt -O modrinth.txt &
+wget -q https://aol.com/ads.txt -O aol.txt &
+wget -q https://www.att.com/ads.txt -O att.txt &
 wait
 # Processing
 base=$(cat *.txt |\
  cut -f1 -d, |\
  grep -v \# |\
+ grep -v subdomain= |\
  tr [:upper:] [:lower:] |\
  sort |\
  uniq |\
